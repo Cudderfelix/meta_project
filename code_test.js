b7a4e2e74aa068558e954ed9766c39771d5bc464
Cudderfelix <ifeachochukafelix@gmail.com>
2026-02-05 17:54:47 +0100

    added js features

--- code_test.js
+++ code_test.js
@@ -1,1 +1,8 @@
 console.log('I am learning JavaScript!');
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+console.log(greet('Alice'));
+const numbers = [1, 2, 3, 4, 5];
+const squaredNumbers = numbers.map(num => num * num);
+console.log(squaredNumbers);    
